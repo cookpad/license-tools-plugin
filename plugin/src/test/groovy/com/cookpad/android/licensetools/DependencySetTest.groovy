3354a51e7436264c409e0dd2033764d1478ebaef
FUJI Goro <goro.fuji@ebookjapan.co.jp>
2016-04-25 11:58:27 +0900

    add tests

---
+++ plugin/src/test/groovy/com/cookpad/android/licensetools/DependencySetTest.groovy
@@ -0,0 +1,59 @@
+package com.cookpad.android.licensetools
+
+import org.junit.Test
+
+class DependencySetTest {
+
+    @Test
+    public void testNotListedInWithWildcardGroup() throws Exception {
+        def licensesYaml = new DependencySet()
+        def dependencies = new DependencySet()
+
+        licensesYaml.add(LibraryInfo.fromYaml([
+                artifact: "com.example:+:+"
+        ]))
+        dependencies.add(LibraryInfo.fromYaml([
+                artifact: "com.example:foo:1.0.0"
+        ]))
+        dependencies.add(LibraryInfo.fromYaml([
+                artifact: "com.example:bar:1.0.0"
+        ]))
+        dependencies.add(LibraryInfo.fromYaml([
+                artifact: "com.example:baz:1.0.0"
+        ]))
+
+        assert dependencies.notListedIn(licensesYaml).empty
+        assert licensesYaml.notListedIn(dependencies).empty
+    }
+
+    @Test
+    public void testNotListedInWithWildcardVersion() throws Exception {
+        def licensesYaml = new DependencySet()
+        def dependencies = new DependencySet()
+
+        licensesYaml.add(LibraryInfo.fromYaml([
+                artifact: "com.example:foo:+"
+        ]))
+        dependencies.add(LibraryInfo.fromYaml([
+                artifact: "com.example:foo:1.0.0"
+        ]))
+
+        assert dependencies.notListedIn(licensesYaml).empty
+    }
+
+
+    @Test
+    public void testNotListedInLibrariesYaml() throws Exception {
+        def licensesYaml = new DependencySet()
+        def dependencies = new DependencySet()
+
+        licensesYaml.add(LibraryInfo.fromYaml([
+                artifact: "com.example:foo:+"
+        ]))
+        dependencies.add(LibraryInfo.fromYaml([
+                artifact: "com.example:bar:1.0.0"
+        ]))
+
+        assert dependencies.notListedIn(licensesYaml).first().artifactId.toString() == "com.example:bar:1.0.0"
+    }
+}
