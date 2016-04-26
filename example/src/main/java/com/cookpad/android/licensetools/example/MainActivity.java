1724d68d1f1842c30df0d51b3549339985468600
FUJI Goro <goro.fuji@ebookjapan.co.jp>
2016-04-26 13:28:12 +0900

    tweaks for examples

--- example/src/main/java/com/cookpad/android/licensetools/example/MainActivity.java
+++ example/src/main/java/com/cookpad/android/licensetools/example/MainActivity.java
@@ -1,5 +1,8 @@
 package com.cookpad.android.licensetools.example;
 
+import com.cookpad.android.licensetools.example.databinding.ActivityMainBinding;
+
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
@@ -11,11 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        WebView webView = (WebView) findViewById(R.id.webView);
-        assert webView != null;
-
+        WebView webView = binding.webView;
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
         webView.loadUrl(LICENSES_HTML_PATH);
