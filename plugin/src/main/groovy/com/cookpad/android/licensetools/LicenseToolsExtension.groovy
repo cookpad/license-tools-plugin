696be17b2b0dcf03810a05cdcd5962d4b51d95d5
Kazuaki Matsuo <matsuo@cookpad.com>
2018-01-15 10:47:11 +0900

    add ignoring resolving rependencies (#75)

--- plugin/src/main/groovy/com/cookpad/android/licensetools/LicenseToolsExtension.groovy
+++ plugin/src/main/groovy/com/cookpad/android/licensetools/LicenseToolsExtension.groovy
@@ -2,13 +2,15 @@ package com.cookpad.android.licensetools
 
 public class LicenseToolsExtension {
 
-    public static String NAME = "licenseTools";
+    public static String NAME = "licenseTools"
 
-    public File licensesYaml = new File("licenses.yml");
+    public File licensesYaml = new File("licenses.yml")
 
-    public File outputHtml = new File("licenses.html");
+    public File outputHtml = new File("licenses.html")
 
-    public File outputJson = new File("licenses.json");
+    public File outputJson = new File("licenses.json")
 
-    public Set<String> ignoredGroups = new HashSet<>();
+    public Set<String> ignoredGroups = new HashSet<>()
+
+    public Set<String> ignoredProjects = new HashSet<>()
 }
