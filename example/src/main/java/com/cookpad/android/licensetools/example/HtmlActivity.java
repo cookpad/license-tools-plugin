8b8f9fb35b2dd9266a9945eb31b242d21297c2d1
Takafumi Kanda <taka-kanda@mail.nissan.co.jp>
2018-10-14 22:52:40 +0530

    Rename MainActivity to HtmlActivity in example

--- example/src/main/java/com/cookpad/android/licensetools/example/HtmlActivity.java
+++ example/src/main/java/com/cookpad/android/licensetools/example/HtmlActivity.java
@@ -1,20 +1,20 @@
 package com.cookpad.android.licensetools.example;
 
-import com.cookpad.android.licensetools.example.databinding.ActivityMainBinding;
+import com.cookpad.android.licensetools.example.databinding.ActivityHtmlBinding;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+public class HtmlActivity extends AppCompatActivity {
 
     private static final String LICENSES_HTML_PATH = "file:///android_asset/licenses.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        ActivityHtmlBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_html);
 
         WebView webView = binding.webView;
         webView.getSettings().setLoadWithOverviewMode(true);
